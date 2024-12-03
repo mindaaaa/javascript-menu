276f59085d4367c604cc36dac332a96b3b5afc33
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-03 18:53:46 +0900

    feat(Category): 이미 먹은 메뉴를 업데이트하는 removeEatenMenu 함수 구현

--- src/domain/Category.js
+++ src/domain/Category.js
@@ -1,10 +1,14 @@
 class Category {
-  constructor(name, menus) {
-    this.name = name;
+  constructor(category, menus) {
+    this.category = category;
     this.menus = menus;
   }
 
-  getAvailableMenus(dislikedMenus, eatenMenus) {
+  removeEatenMenu(eatenMenu) {
+    this.menus = this.menus.filter((menu) => menu !== eatenMenu);
+  }
+
+  getMenus(dislikedMenus, eatenMenus) {
     return this.menus
       .filter((menu) => !dislikedMenus.includes(menu))
       .filter((menu) => !eatenMenus.includes(menu));
