35df5633fc576cc96a8133e68e2ab3d77926e536
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-03 19:37:05 +0900

    feat(Category): getAvaliableMenus 로직 수정

--- src/domain/Category.js
+++ src/domain/Category.js
@@ -1,16 +1,20 @@
 class Category {
+  // 먹은 메뉴 지워주기
   constructor(category, menus) {
     this.category = category;
     this.menus = menus;
   }
 
-  removeEatenMenu(eatenMenu) {
-    this.menus = this.menus.filter((menu) => menu !== eatenMenu);
-  }
+  getAvailableMenus(coach) {
+    const dislikedMenus = coach.getDislikedMenusByCategory(this.category);
+    const eatenMenus = coach.eatenMenus || [];
 
-  getMenus(dislikedMenus, eatenMenus) {
     return this.menus
       .filter((menu) => !dislikedMenus.includes(menu))
       .filter((menu) => !eatenMenus.includes(menu));
+  }
+
+  removeEatenMenu(eatenMenu) {
+    this.menus = this.menus.filter((menu) => menu !== eatenMenu);
   }
 }
