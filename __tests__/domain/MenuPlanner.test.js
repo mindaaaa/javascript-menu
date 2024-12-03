1f5dc8e560d0da6edc167d16490e25bb1f215f5a
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-03 21:46:01 +0900

    test(MenuPlanner): 카테고리에서 랜덤 메뉴를 선택하는 MenuPlanner 단위 테스트 코드 작성

---
+++ __tests__/domain/MenuPlanner.test.js
@@ -0,0 +1,25 @@
+const mockShuffles = (rows) => {
+  MissionUtils.Random.shuffle = jest.fn();
+
+  rows.reduce((acc, [firstNumber, numbers]) => {
+    return acc.mockReturnValueOnce([
+      firstNumber,
+      ...numbers.filter((number) => number !== firstNumber),
+    ]);
+  }, MissionUtils.Random.shuffle);
+};
+
+describe('랜덤 섞기 테스트', () => {
+  test('shuffle 동작을 모킹하여 예상 결과를 확인한다.', () => {
+    mockShuffles([
+      [1, [1, 2, 3]],
+      [2, [2, 3, 4]],
+    ]);
+
+    const result1 = MissionUtils.Random.shuffle([1, 2, 3]);
+    const result2 = MissionUtils.Random.shuffle([2, 3, 4]);
+
+    expect(result1).toEqual([1, 2, 3]);
+    expect(result2).toEqual([2, 3, 4]);
+  });
+});
--- src/domain/MenuPlanner.js
+++ src/domain/MenuPlanner.js
@@ -1,16 +1,39 @@
+import MenuPlanner from '../src/domain/MenuPlanner.js';
+import Coach from '../src/domain/Coach.js';
 import { Randoms } from '@woowacourse/mission-utils';
 
-class MenuPlanner {
-  constructor(coach) {
-    this.coach = coach;
-  }
+jest.mock('@woowacourse/mission-utils', () => ({
+  Randoms: {
+    shuffle: jest.fn(),
+  },
+}));
 
-  planMenu(category) {
-    const availableMenus = this.coach.getAvailableMenus();
-    const categoryMenus = availableMenus[category];
+describe('MenuPlanner 클래스 테스트', () => {
+  let coach, menuPlanner;
 
-    return Randoms.shuffle(categoryMenus)[0];
-  }
-}
+  beforeEach(() => {
+    // Coach
+    coach = new Coach('토미');
+    coach.getAvailableMenus = jest.fn(() => ({
+      한식: ['김밥', '비빔밥', '불고기'],
+      일식: ['스시', '라멘'],
+    }));
 
-export default MenuPlanner;
+    // MenuPlanner
+    menuPlanner = new MenuPlanner(coach);
+  });
+
+  test('MenuPlanner 생성자가 초기값을 올바르게 설정한다.', () => {
+    expect(menuPlanner.coach).toBe(coach);
+  });
+
+  test('planMenu는 카테고리에서 랜덤으로 메뉴를 반환한다.', () => {
+    Randoms.shuffle.mockReturnValue(['김밥', '비빔밥', '불고기']);
+
+    const category = '한식';
+    const recommendedMenu = menuPlanner.planMenu(category);
+
+    expect(Randoms.shuffle).toHaveBeenCalledWith(['김밥', '비빔밥', '불고기']);
+    expect(recommendedMenu).toBe('김밥');
+  });
+});
