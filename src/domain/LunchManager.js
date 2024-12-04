5d21941ac3ce1d18e0be130f53de2717b9a418f1
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-12-04 19:46:28 +0900

    feat(LunchManager): 수정된 클래스들을 바탕으로 연계하는 로직 업데이트

--- src/domain/LunchManager.js
+++ src/domain/LunchManager.js
@@ -2,40 +2,15 @@ import CategoryPicker from './CategoryPicker.js';
 import MenuPlanner from './MenuPlanner.js';
 // 하루 단위의 로직 흐름(카테고리 , 메뉴 추천 , 기록)을 담당한다.
 class LunchManager {
-  constructor(coaches, categories) {
+  constructor(coaches) {
     this.coaches = coaches;
-    this.categories = categories;
     this.categoryPicker = new CategoryPicker(this.getDailyCategoryCount());
     this.result = [];
-
-    this.initializeCategories();
   }
-
-  getDailyCategoryCount() {
-    return this.categories.reduce((acc, category) => {
-      acc[category] = 0;
-      return acc;
-    }, {}); // 카테고리별 사용 횟수
-  }
-
-  initializeCategories() {
-    this.coaches.forEach((coach) => {
-      this.categories.forEach((category) => {
-        const dislikedMenus = coach.getDislikedMenusByCategory(category.name);
-        dislikedMenus.forEach((menu) => {
-          category.removeMenu(menu); // 카테고리에서 못 먹는 메뉴 제거
-        });
-      });
-    });
-  }
-
-  // Object.entries로도 풀어보기
 
   recommendDailyMenu() {
-    const categoryName = this.categoryPicker.pickCategory();
-    const category = this.categories.find(
-      (category) => category.name === categoryName
-    );
+    // 카테고리 선택
+    const category = this.categoryPicker.pickCategory();
     const dailyMenus = {};
 
     // 코치별 메뉴 추천
@@ -44,12 +19,11 @@ class LunchManager {
       const recommendedMenu = menuPlanner.planMenu(category);
       dailyMenus[coach.name] = recommendedMenu;
 
-      coach.addEatenMenu(recommendedMenu); // 코치 상태 업데이트
-      category.removeMenu(recommendedMenu); // 카테고리 상태 업데이트
+      coach.removeEatenMenu(category, recommendedMenu); // 코치 상태 업데이트
     });
 
-    this.result.push({ category: category.name, menus: dailyMenus });
-    return { category: category.name, menus: dailyMenus };
+    this.result.push({ category, menus: dailyMenus });
+    return { category, menus: dailyMenus };
   }
 
   startRecommendation(days = 5) {
